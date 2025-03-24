ccb2eda8827b722ce32a250026f9c2963213104b
admin <admin@felipechagas.altostrat.com>
2025-03-24 15:34:20 +0000

    Config

--- config.py
+++ config.py
@@ -24,7 +24,7 @@
 
 # Use a configuration class or dictionary for better organization
 class Config:
-    PROJECT_ID = 'ce-sap-latam-test-deploy'
+    PROJECT_ID = 'your-project-id'
     RLS_MANAGER_DATASET = 'rls_security'
     POLICY_TABLE = f'{PROJECT_ID}.{RLS_MANAGER_DATASET}.policies'
     FILTER_TABLE = f'{PROJECT_ID}.{RLS_MANAGER_DATASET}.policies_filters'
